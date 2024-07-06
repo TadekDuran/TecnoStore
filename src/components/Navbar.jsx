f8a1cc513418209f0af859e31854cd4d7c0a3821
Tadek <tadekduran@gmail.com>
2024-07-06 17:17:06 -0300

    crear links en navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,36 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="sticky top-0 flex h-11 justify-center bg-white/[.8] text-black">
       <div className="container flex items-center justify-between">
-        <button>TS</button>
+        <Link href={'/'}>TS</Link>
         <div className="flex gap-4">
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          <Link href={'/destacados'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Destacados
-          </button>
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          </Link>
+          <Link href={'/smartphones'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Smartphones
-          </button>
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          </Link>
+          <Link href={'/tablets'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Tablets
-          </button>
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          </Link>
+          <Link href={'/notebooks'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Notebooks
-          </button>
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          </Link>
+          <Link href={'/accesorios'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Accesorios
-          </button>
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          </Link>
+          <Link href={'/consolas'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Consolas
-          </button>
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          </Link>
+          <Link href={'/camaras'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Cámaras
-          </button>
-          <button className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
+          </Link>
+          <Link href={'/drones'} className="hover:text-[#2696ff] hover:underline hover:decoration-[#ff9d00] hover:underline-offset-4">
             Drones
-          </button>
+          </Link>
         </div>
       </div>
     </nav>
