1350af01a68e7a86602c5a021c5dcc0bde8648e9
Tadek <tadekduran@gmail.com>
2024-07-08 15:53:38 -0300

    crear ProductCard y fetch de base de datos

--- src/app/api/routes/products/[id]/route.js
+++ src/app/api/routes/products/[id]/route.js
@@ -1,51 +1,90 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "../../../utils/database";
-import Product from '../../../models/Product'
+import Product from "../../../models/Product";
 
-export async function GET(request, { params }) {
-    connectDB()
-    try {
-        const foundProduct = await Product.findById(params.id)
+export async function GET(request, context) {
+  const { params } = context;
+  if (params.id) {
+    return getProductById(request, context);
+  } else {
+    return getProductsByCategory(request);
+  }
+}
 
-        if (!foundProduct) return NextResponse.json({
-            status: 404,
-            message: 'No se encontró el producto con el ID especificado.'
-        })
+async function getProductById(request, { params }) {
+  connectDB();
+  try {
+    const foundProduct = await Product.findById(params.id);
 
-        return NextResponse.json(foundProduct);
-    } catch (error) {
-        return NextResponse.json(error.message, {
-            status: 400,
-            message: error.message
-        })
+    if (!foundProduct)
+      return NextResponse.json({
+        status: 404,
+        message: "No se encontró el producto con el ID especificado.",
+      });
+
+    return NextResponse.json(foundProduct);
+  } catch (error) {
+    return NextResponse.json({
+      status: 400,
+      message: error.message,
+    });
+  }
+}
+
+// Función para obtener productos por categoría
+async function getProductsByCategory(request) {
+  connectDB();
+  try {
+    const { searchParams } = request.nextUrl.searchParams;
+    const category = searchParams.get("categoria");
+
+    let query = {};
+    if (category) {
+      query.categoria = category;
     }
+
+    const foundProducts = await Product.find(query);
+
+    if (!foundProducts.length)
+      return NextResponse.json({
+        status: 404,
+        message: "No se encontraron productos con la categoría especificada.",
+      });
+
+    return NextResponse.json(foundProducts);
+  } catch (error) {
+    return NextResponse.json({
+      status: 400,
+      message: error.message,
+    });
+  }
 }
 
 export async function PUT(request, { params }) {
-    try {
-        const data = await request.json();
-        const updatedProduct = await Product.findByIdAndUpdate(params.id, data, {
-            new: true
-        })
-        return NextResponse.json(updatedProduct)
-    } catch (error) {
-        return NextResponse.json(error.message, {
-            status: 400
-        })
-    }
+  try {
+    const data = await request.json();
+    const updatedProduct = await Product.findByIdAndUpdate(params.id, data, {
+      new: true,
+    });
+    return NextResponse.json(updatedProduct);
+  } catch (error) {
+    return NextResponse.json(error.message, {
+      status: 400,
+    });
+  }
 }
 
 export async function DELETE(request, { params }) {
-    try {
-        await Product.findByIdAndDelete(params.id)
+  try {
+    await Product.findByIdAndDelete(params.id);
 
-        return NextResponse.json({
-            status: 200,
-            message: 'Producto eliminado'
-        });
-    } catch (error) {
-        return NextResponse.json(error.message, {
-            status: 400
-        })
-    }
+    return NextResponse.json({
+      status: 200,
+      message: "Producto eliminado",
+    });
+  } catch (error) {
+    return NextResponse.json(error.message, {
+      status: 400,
+    });
+  }
 }
--- src/app/smartphones/page.jsx
+++ src/app/smartphones/page.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from "react";
+import ProductCard, { getProductsByCategory } from "@/components/ProductCard";
 
-const Smartphones = () => {
-  return (
-    <div>Smartphones</div>
-  )
+async function getSmartphones() {
+  return await getProductsByCategory('Smartphone');
 }
 
-export default Smartphones
+const Smartphones = () => {
+  const [smartphones, setSmartphones] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchSmartphones() {
+      try {
+        const data = await getSmartphones();
+        if (Array.isArray(data)) {
+          setSmartphones(data);
+        } else {
+          throw new Error('Data format is incorrect');
+        }
+        console.log(data); // Log the data to check its structure
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchSmartphones();
+  }, []);
+
+  if (loading) return <div>Cargando...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (smartphones.length === 0) return <div>No se encontraron productos</div>;
+
+  return (
+    <div>
+      {smartphones.map((smartphone) => (
+        <ProductCard key={smartphone._id} smartphone={smartphone} />
+      ))}
+    </div>
+  );
+};
+
+export default Smartphones;
